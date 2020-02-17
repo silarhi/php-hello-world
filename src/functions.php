a9be3e94f46df1842475175982b86cb85aef8765
Guillaume Sainthillier <guillaume.sainthillier@gmail.com>
2020-02-17 17:59:52 +0100

    Revert "Welcome v1.4"
    This reverts commit 1831aa1596c8097e348f6827cc06703cbbef9061.

--- src/functions.php
+++ src/functions.php
@@ -2,7 +2,7 @@
 
 namespace Silarhi;
 
-define('HELLO_WORLD_VERSION', '1.4.0');
+define('HELLO_WORLD_VERSION', '1.3.0');
 
 function hello() {
 	return sprintf("Hello World v%s", HELLO_WORLD_VERSION);
