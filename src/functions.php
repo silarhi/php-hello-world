c2cbb67fbb1f24ed309d999be387fde561958a86
Guillaume Sainthillier <guillaume.sainthillier@gmail.com>
2020-02-17 17:36:34 +0100

    First draft of hello-world

---
+++ src/functions.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Silarhi;
+
+define('HELLO_WORLD_VERSION', '1.0');
+
+function hello() {
+	return sprintf("Hello World v%s", HELLO_WORLD_VERSION);
+}
---
+++ tests/HelloWorldTest.php
@@ -0,0 +1,13 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use function Silarhi\hello;
+
+final class HelloWorldTest extends TestCase
+{
+	public function testHello()
+    {
+    	$contents = hello();
+        $this->assertEquals('Hello World v1.0', $contents);
+    }
+}
