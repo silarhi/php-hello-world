3b654eb9641d40f680c284c8bc0d83f77b998cc0
Guillaume Sainthillier <guillaume.sainthillier@gmail.com>
2020-02-17 17:47:08 +0100

    Upgrade lib version

--- src/functions.php
+++ src/functions.php
@@ -2,7 +2,7 @@
 
 namespace Silarhi;
 
-define('HELLO_WORLD_VERSION', '1.0');
+define('HELLO_WORLD_VERSION', '1.0.1');
 
 function hello() {
 	return sprintf("Hello World v%s", HELLO_WORLD_VERSION);
