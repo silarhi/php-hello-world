a5f5b3d79c0c19420f53bae63cc51185c79439ea
Guillaume Sainthillier <guillaume.sainthillier@gmail.com>
2020-02-17 17:56:16 +0100

    Welcome V1.2

--- src/functions.php
+++ src/functions.php
@@ -2,7 +2,7 @@
 
 namespace Silarhi;
 
-define('HELLO_WORLD_VERSION', '1.1.0');
+define('HELLO_WORLD_VERSION', '1.2.0');
 
 function hello() {
 	return sprintf("Hello World v%s", HELLO_WORLD_VERSION);
