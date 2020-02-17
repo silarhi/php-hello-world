da46a71c3577f01878ccb7a1a2bf76e0430a9458
Guillaume Sainthillier <guillaume.sainthillier@gmail.com>
2020-02-17 17:48:17 +0100

    Fixed tests

--- src/functions.php
+++ src/functions.php
@@ -2,7 +2,7 @@
 
 namespace Silarhi;
 
-define('HELLO_WORLD_VERSION', '1.0.1');
+define('HELLO_WORLD_VERSION', '1.0.2');
 
 function hello() {
 	return sprintf("Hello World v%s", HELLO_WORLD_VERSION);
