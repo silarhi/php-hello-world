f64076505f2e7657ac2ba4ab1f7f13342eb7ce5f
Guillaume Sainthillier <guillaume.sainthillier@gmail.com>
2020-02-18 13:52:59 +0100

    Added PHP CS

--- src/functions.php
+++ src/functions.php
@@ -1,9 +1,17 @@
 <?php
 
+/*
+ * This file is part of Silarhi.
+ * (c) Guillaume Sainthillier <hello@silarhi.fr>
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace Silarhi;
 
-define('HELLO_WORLD_VERSION', '1.5.0');
+\define('HELLO_WORLD_VERSION', '1.5.0');
 
-function hello() {
-	return sprintf("Hello World v%s", HELLO_WORLD_VERSION);
+function hello()
+{
+    return sprintf('Hello World v%s', HELLO_WORLD_VERSION);
 }
--- tests/HelloWorldTest.php
+++ tests/HelloWorldTest.php
@@ -1,13 +1,20 @@
 <?php
 
+/*
+ * This file is part of Silarhi.
+ * (c) Guillaume Sainthillier <hello@silarhi.fr>
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 use PHPUnit\Framework\TestCase;
 use function Silarhi\hello;
 
 final class HelloWorldTest extends TestCase
 {
-	public function testHello()
+    public function testHello()
     {
-    	$contents = hello();
+        $contents = hello();
         $this->assertEquals('Hello World v1.5.0', $contents);
     }
 }
