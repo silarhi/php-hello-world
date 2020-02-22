78d67bde7823e13e87f42093e64db16dcdd6ccd4
Guillaume Sainthillier <guillaume.sainthillier@gmail.com>
2020-02-22 15:02:58 +0100

    Welcome v1.5.1

--- tests/HelloWorldTest.php
+++ tests/HelloWorldTest.php
@@ -1,13 +1,23 @@
 <?php
 
+/*
+ * This file is part of Silarhi.
+ * (c) Guillaume Sainthillier <hello@silarhi.fr>
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Silarhi\Tests;
+
 use PHPUnit\Framework\TestCase;
-use function Silarhi\hello;
+use Silarhi\Hello;
 
 final class HelloWorldTest extends TestCase
 {
-	public function testHello()
+    public function testHello()
     {
-    	$contents = hello();
-        $this->assertEquals('Hello World v1.5.0', $contents);
+        $hello = new Hello();
+        $contents = $hello->display();
+        $this->assertEquals('Hello World v1.5.1', $contents);
     }
 }
